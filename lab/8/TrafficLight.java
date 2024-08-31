31f3fe1500a0e1af89a7ed8c6932165629ab79d6
MILAN BHADARKA <bhadarkamilan3@gmail.com>
2024-08-31 18:57:42 +0530

    31-08-2024

--- lab/8/TrafficLight.java
+++ lab/8/TrafficLight.java
@@ -8,6 +8,8 @@ public class TrafficLight extends Frame implements ItemListener {
     Color Yellowcolor = Color.WHITE;
     Color Greencolor = Color.WHITE;
 
+    boolean gameend = false;
+
     CheckboxGroup cbg = new CheckboxGroup();
 
 
