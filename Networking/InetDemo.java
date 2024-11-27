c5cb0e916ae7c9f5c7e5b80ea005ea9fa95ba5aa
MILAN BHADARKA <bhadarkamilan3@gmail.com>
2024-11-27 09:28:17 +0530

    27-11-2024

--- Networking/InetDemo.java
+++ Networking/InetDemo.java
@@ -2,7 +2,7 @@ import java.net.*;
 public class InetDemo{  
 public static void main(String[] args){  
 try{  
-InetAddress ip=InetAddress.getByName("www.railway.app");  
+MyInetAddress ip=MyInetAddress.getByName("www.railway.app");  
   
 System.out.println("Host Name: "+ip.getHostName());  
 System.out.println("IP Address: "+ip.getHostAddress());  
---
+++ Networking/MyInetAddress.java
@@ -0,0 +1,18 @@
+import java.net.*;
+
+public class MyInetAddress {
+    public static void main(String[] args){
+
+        try{
+        
+            InetAddress ip = InetAddress.getByName("moneymate.up.railway.app");
+        
+            System.out.println("Host Name: " + ip.getHostName());
+            
+            System.out.println("IP Address: " + ip.getHostAddress());
+            
+            } catch(Exception e) {
+                System.out.println(e);
+            }
+}
+}
